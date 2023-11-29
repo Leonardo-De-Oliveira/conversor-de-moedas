f605b786d498010dc692be57289ec9e03753439f
Leonardo De Oliveira <leonardodeoliveira@outlook.com>
2023-11-29 20:08:08 -0300

    refatorando código e inserindo opções novas no input

--- script.js
+++ script.js
@@ -1,87 +1,147 @@
-function writeScreen(conversao, moeda) {
-    console.log(conversao, moeda)
+let coin = document.getElementById("coin")
+let coin_2 = document.getElementById("coin-2")
 
-    let real = document.getElementById("real").value
+let coinSelect 
+let resultado
 
-    document.querySelector(".valor-selecionado").innerHTML = document.querySelector(".valor-2").innerHTML = new Intl.NumberFormat("pt-BR", {
-        style: "currency",
-        currency: "BRL"
-    }).format(real) 
+let img = document.getElementById("value-1")    
+let img_2 = document.getElementById("value-4")
 
-    let convertido = ""
-    
-    if(moeda == 'USD-BRL') {
-        convertido = conversao.USDBRL.high
-    } else if(moeda == 'EUR-BRL') {
-        convertido = conversao.EURBRL.high
-    } else {
-        convertido = conversao.BTCBRL.high
-    }
+coin.addEventListener("change", changeValues)
+coin_2.addEventListener("change", changeValues)
 
-    if(convertido != ""){
-        let resultado = real / convertido 
-    
-        if(moeda == 'USD-BRL') {
-            document.querySelector(".valor-2").innerHTML = new Intl.NumberFormat("en-US", {
-                style: "currency",
-                currency: "USD"
-            }).format(resultado) 
-        } else if(moeda == 'EUR-BRL') {
-            document.querySelector(".valor-2").innerHTML = new Intl.NumberFormat("de-DE", {
-                style: "currency",
-                currency: "EUR"
-            }).format(resultado) 
-        } else {
-            document.querySelector(".valor-2").innerHTML = new Intl.NumberFormat("en-US", {
-                style: "currency",
-                currency: "BTC"
-            }).format(resultado) 
-        }
-    }
-}
+async function valueCoin() {
 
-async function valueDolar(moeda) {
-    let conversao = await fetch ("http://economia.awesomeapi.com.br/json/last/" +
-    moeda + "/"
+    let data = await fetch("https://economia.awesomeapi.com.br/last/" +
+    coin_2.value
     ).then(resposta => resposta.json())
 
-    writeScreen(conversao, moeda)
+    let euro = await fetch("https://economia.awesomeapi.com.br/last/" +
+    "EUR-BRL"
+    ).then(resposta => resposta.json())
+
+    let euroBrl = euro.EURBRL.high
+    
+    convertCoin(data, euroBrl)
 }
 
-function converterMoeda() {
-    let moeda = document.getElementById("moedas").value
+function convertCoin(data, euroBrl){
+    let insert = document.getElementById("insert").value
 
-    valueDolar(moeda)
-}
+    if(coin_2.value == "USD-BRL") {
+        coinSelect = data.USDBRL.high
+    }
+    if(coin_2.value == "EUR-BRL") {
+        coinSelect = data.EURBRL.high
+    }
+    if(coin.value == "USD-BRL" && coin_2.value == "BRL-USD") {
+        coinSelect = data.BRLUSD.high
+    }
+    if(coin_2.value == "BRL-USD" && coin.value == "EUR-BRL") {
+        coinSelect = euroBrl
+    }
+    
+    console.log(coinSelect)
 
-function change() {
-    let moedaSelecionada = document.getElementById("moedas").value
-
-    if(moedaSelecionada == "USD-BRL"){
-        document.querySelector(".valor-1").innerHTML = "Dólar Americano"
-        document.querySelector(".valor-2").innerHTML = "U$ "
-        
-        let image = document.getElementById("valor-3")
-
-        image.src = "./assets/us.png"
-
-    } else if(moedaSelecionada == "EUR-BRL"){
-        document.querySelector(".valor-1").innerHTML = "Euro"
-        document.querySelector(".valor-2").innerHTML = "Є "
-
-        let image = document.getElementById("valor-3")
-
-        image.src = "./assets/eu.png"
+    if(coin.value === coin_2.value) {
+        resultado = insert
     } else {
-        document.querySelector(".valor-1").innerHTML = "Bitcoin"
-        document.querySelector(".valor-2").innerHTML = "BTC "
-        
-        let image = document.getElementById("valor-3")
+        if(coin.value == "BRL-USD" && coin_2.value == "USD-BRL") {
+            resultado = insert / coinSelect
+        } 
+        else if(coin.value == "USD-BRL" && coin_2.value == "EUR-BRL" || coin.value == "EUR-BRL" && coin_2.value == "USD-BRL") {
+            resultado = 0
+        } 
+        else if(coin.value == "USD-BRL" && coin_2.value == "BRL-USD") {
+            resultado = insert / coinSelect
+        } 
+        else if (coin.value == "BRL-USD" && coin_2.value == "EUR-BRL") {
+            resultado = coinSelect / insert
+        } 
+        else if (coin_2.value == "BRL-USD" && coin.value == "EUR-BRL"){
+            resultado = insert * coinSelect 
+        }
+    
+    }
 
-        image.src = "./assets/bitcoin.png"
+    console.log(resultado)
+    console.log(insert)
+
+    formatValues(insert, resultado)    
+
+}
+
+function formatValues(insert, resultado) {
+    if(coin.value == "USD-BRL"){
+        document.querySelector(".value-3").innerHTML = new Intl.NumberFormat("en-US", { 
+            style: 'currency', 
+            currency: 'USD' 
+        }).format(insert)
+    }
+    if(coin_2.value == "USD-BRL"){
+        document.querySelector(".value-6").innerHTML = new Intl.NumberFormat("en-US", { 
+            style: 'currency', 
+            currency: 'USD' 
+        }).format(resultado)
+    }
+    if(coin.value == "BRL-USD"){
+        document.querySelector(".value-3").innerHTML = new Intl.NumberFormat("pt-BR", { 
+            style: 'currency', 
+            currency: 'BRL' 
+        }).format(insert)
+    }
+    if(coin_2.value == "BRL-USD"){
+        document.querySelector(".value-6").innerHTML = new Intl.NumberFormat("pt-BR", { 
+            style: 'currency', 
+            currency: 'BRL' 
+        }).format(resultado)
+    }
+    if(coin.value == "EUR-BRL"){
+        document.querySelector(".value-3").innerHTML = new Intl.NumberFormat("de-DE", { 
+            style: 'currency', 
+            currency: 'EUR' 
+        }).format(insert)
+    }
+    if(coin.value == "BRL-USD" && coin_2.value == "EUR-BRL"){
+        document.querySelector(".value-6").innerHTML = new Intl.NumberFormat("de-DE", { 
+            style: 'currency', 
+            currency: 'EUR' 
+        }).format(resultado)
     }
 }
 
-const select = document.querySelector("#moedas")
+function changeValues() {    
 
-select.addEventListener("change", change)
+    if(coin.value === "BRL-USD") {
+        img.src = "./assets/brasil.png"
+        document.querySelector(".value-2").innerHTML = "Real Brasileiro"
+        document.querySelector(".value-3").innerHTML = "R$"
+    }
+    if(coin_2.value === "BRL-USD") {
+        img_2.src = "./assets/brasil.png"
+        document.querySelector(".value-5").innerHTML = "Real Brasileiro"
+        document.querySelector(".value-6").innerHTML = "R$"
+    }   
+    if(coin.value === "USD-BRL") {
+        img.src = "./assets/us.png"
+        document.querySelector(".value-2").innerHTML = "Dólar americano"
+        document.querySelector(".value-3").innerHTML = "$"
+    }
+    if(coin_2.value === "USD-BRL") {
+        img_2.src = "./assets/us.png"
+        document.querySelector(".value-5").innerHTML = "Dólar americano"
+        document.querySelector(".value-6").innerHTML = "$"
+    }
+    if(coin.value === "EUR-BRL") {
+        img.src = "./assets/eu.png"
+        document.querySelector(".value-2").innerHTML = "Euro"
+        document.querySelector(".value-3").innerHTML = "Є"
+    }
+    if(coin_2.value === "EUR-BRL") {
+        img_2.src = "./assets/eu.png"
+        document.querySelector(".value-5").innerHTML = "Euro"
+        document.querySelector(".value-6").innerHTML = "Є"
+    }   
+
+}
+
